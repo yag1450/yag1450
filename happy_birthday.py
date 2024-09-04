7a85c15c7090a1621ed399f784132957e6f37247
yag1450 <yag1450@g.rit.edu>
2024-09-04 14:57:08 +0400

    Add files via upload

---
+++ happy_birthday.py
@@ -0,0 +1,10 @@
+def bday_message():
+    name = input("Enter your name ")
+    month = input("Enter your birth month ")
+    day = input("Enter your birth day of the month ")
+    year = input("Enter your birth year ")
+    
+    print(name,"your birthday is on", month, day, year)
+bday_message()
+bday_message()
+bday_message()
