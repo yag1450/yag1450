5d90f119412a268edc768255e076e594b7ce5d29
yag1450 <yag1450@g.rit.edu>
2024-09-07 18:38:21 +0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,12 @@
+def variable_practice():
+    year = 12
+    age_in_months = int(input("Whats your age: "))
+    print("Your age in months is:", age_in_months * year)
+    month = 30
+    days_in_year = int(year * month)
+    print("The number of days in a year is:", (month * year)+5)
+    name_of_first_pet = str = "Leo"
+    print("The name of your first pet:", name_of_first_pet)
+    first_five_digits_pi = float = 3.1415
+    print("The first five digits of Pi:", first_five_digits_pi)
+variable_practice()
