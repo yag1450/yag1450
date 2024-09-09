257728270eb01b777fa6b82e5ce11de69fefdc9a
yag1450 <yag1450@g.rit.edu>
2024-09-09 15:46:29 +0400

    Add files via upload

---
+++ knock_knock.py
@@ -0,0 +1,6 @@
+def joke():
+    print("Knock Knock!")
+    who_there = input("Who's there? ")
+    print(who_there, "who?")
+    print("I didn't know that a computer program can make jokes!")
+joke()
